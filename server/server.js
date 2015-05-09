60bf00a767bbd1def9534e2c39b69a2bb8f26d15
John Winstead <awatemonosan@gmail.com>
2015-05-08 18:48:03 -0700

    Fix static routing for shared and alias for client

--- server/server.js
+++ server/server.js
@@ -9,9 +9,9 @@ app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + './../client'));
-app.use(express.static('/client', __dirname + './../client'));
-app.use(express.static('/shared', __dirname + './../shared'));
+app.use(express.static(__dirname + '/../client'));
+app.use('/client', express.static(__dirname + '/../client'));
+app.use('/shared', express.static(__dirname + '/../shared'));
 
 app.listen(app.get('port'), function() {
   console.log('MineCafe running on port ' + app.get('port'));
