488022b74962007940d59cd42d0a7a54a95743ca
John Winstead <awatemonosan@gmail.com>
2015-05-08 18:34:50 -0700

    add path for shared and an alias for client

--- server/server.js
+++ server/server.js
@@ -8,7 +8,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname + './../client'));
+app.use(express.static('/client', __dirname + './../client'));
+app.use(express.static('/shared', __dirname + './../shared'));
 
 app.listen(app.get('port'), function() {
   console.log('MineCafe running on port ' + app.get('port'));
