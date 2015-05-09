9bac3e18f29f96b3ec37fb6bc2501e77c359d257
John Winstead <awatemonosan@gmail.com>
2015-05-08 18:30:00 -0700

    Move this.list into a closure instead

--- shared/compliment.js
+++ shared/compliment.js
@@ -1,8 +1,10 @@
-exports= function(){
-  return this.list[Math.floor(Math.random()*this.list.length)];
-}
-exports.list = [
-"I like your style.",
-"I can't wait to see what you build!",
-"You are looking amazing today, by the way."
-];
+module.exports = (function(){
+  var list = [
+    "I like your style.",
+    "I can't wait to see what you build!",
+    "You are looking amazing today, by the way."
+  ];
+  return function(){
+    return list[Math.floor(Math.random()*list.length)];
+  }
+})();
